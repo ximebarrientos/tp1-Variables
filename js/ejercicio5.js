1eb46ceb3da83820b26d6b5c1a96d630e6019df0
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-04-13 23:31:29 -0300

    ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,4 @@
+const num1= parseInt(prompt('Ingrese un número'))
+const num2= parseInt(prompt("Ingrese un segundo número"))
+
+document.writeln("El Resultado de la suma de esos dos números es: "+ (num1+num2))
